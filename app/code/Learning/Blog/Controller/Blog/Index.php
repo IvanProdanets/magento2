441c72da218bf249d8223390249372f05a6ccadd
Prodanets Ivan <ivan.prodanets@transoftgroup.com>
2020-04-03 14:53:40 +0300

    M2-66639. Added InstallSchema

--- app/code/Learning/Blog/Controller/Blog/Index.php
+++ app/code/Learning/Blog/Controller/Blog/Index.php
@@ -1,14 +1,14 @@
 <?php
 namespace Learning\Blog\Controller\Blog;
 
-use Magento\Framework\App\ActionInterface;
+use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\ResponseInterface;
 
 /**
  * Class Index
  * @package Learning\Blog\Controller\Blog
  */
-class Index implements ActionInterface
+class Index extends Action
 {
     public function execute(): ResponseInterface
     {
